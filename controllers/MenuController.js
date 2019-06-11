a33396785d20259c4132bf3e1b3eb81ffd063663
Zachary Guerrin <39317224+zguerrin27@users.noreply.github.com>
2019-06-11 09:47:39 -0400

    Checkpoint-3: Completed

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -1,4 +1,5 @@
 const inquirer = require('inquirer');
+const ContactController = require("./ContactController");
 
 module.exports = class MenuController {
   constructor(){
@@ -14,8 +15,7 @@ module.exports = class MenuController {
         ]
       }
     ];
-    this.contacts = [];
-    // this.reminder = "Learning is a life-long pursuit";
+    this.book = new ContactController();
   }
 
   main(){
@@ -46,8 +46,15 @@ module.exports = class MenuController {
 
   addContact(){
     this.clear();
-    console.log('addContact called');
-    this.main();
+    inquirer.prompt(this.book.addContactQuestions).then((answers) => {
+      this.book.addContact(answers.name, answers.phone).then((contact) => {
+        console.log("Contact added successfully!");
+        this.main();
+      }).catch((err) => {
+        console.log(err);
+        this.main();
+      });
+    });
   }
 
   getDate(){
@@ -63,7 +70,6 @@ module.exports = class MenuController {
   }
 
   remindMe(){
-    // return this.reminder;
     return "Learning is a life-long pursuit";
   }
 
--- spec/menu_controller_spec.js
+++ spec/menu_controller_spec.js
@@ -6,18 +6,7 @@ describe("MenuController", () => {
     this.menu = new MenuController();
   });
 
-  describe("#getContactCount()", () => {
-
-    it("should return 0 when no contacts are in the book", () => {
-      expect(this.menu.getContactCount()).toBe(0);
-    });
-
-    it("should return 1 when there is exactly one contact in the book", () => {
-      this.menu.contacts.push("Bob");
-      expect(this.menu.getContactCount()).toBe(1)
-    });
-
-  });
+  
 
   describe("#remindMe()", () => {
 
